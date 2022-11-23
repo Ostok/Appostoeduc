5e299a64ea2a40e4ce9d2c94a83a491d3a86a89c
pesto <pestonestor@gmail.com>
2022-11-23 19:58:44 -0300

    agregoo template biologia1

--- educ/urls.py
+++ educ/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="inicio"),
+    path('bio1',views.bio1, name="bio1"),
+    path('bio2',views.bio2, name="bio2"),
+    path('bio3',views.bio3, name="bio3"),
+    path('eco4',views.eco4, name="eco4"),
 ]
--- educ/views.py
+++ educ/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'educ/publica/index.html')
+    
+def bio1(request):
+    return render(request,'educ/publica/bio1.html')
+    
+def bio2(request):
+    return render(request,'educ/publica/bio3.html')
+    
+def bio3(request):
+    return render(request,'educ/publica/bio3.html')
+    
+def eco4(request):
+    return render(request,'educ/publica/eco4.html')
