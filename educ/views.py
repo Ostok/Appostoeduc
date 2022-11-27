7d8141cb6650fd4d3f180b1a196238c82d87540f
pesto <pestonestor@gmail.com>
2022-11-27 17:54:22 -0300

    agrego filter y forloop a bio1eje1

--- educ/views.py
+++ educ/views.py
@@ -7,20 +7,40 @@ def index(request):
 def bio1(request):
     return render(request,'educ/publica/bio1.html')
     
+    
 def bio2(request):
     return render(request,'educ/publica/bio3.html')
+    
     
 def bio3(request):
     return render(request,'educ/publica/bio3.html')
     
+    
 def eco4(request):
     return render(request,'educ/publica/eco4.html')
 
+
 def bio1eje1(request):
-    return render(request,'educ/publica/bio1eje1.html')
+    lista_temas = [
+        'caracterización de los seres vivos.',
+        'concepto de sistema.',
+        'niveles de organizacion de la materia.',
+        'unidad y diversidad de funciones y estructuras.',
+        'biodiversidad: aspectos evolutivos.',
+        'arboles de parentezco entre los seres vivos.',
+        'el origen de la vida.',
+        'origen de células eucariotas. Teoría endosimbiótica.',
+        'concepto general de célula procariota y eucariota.',
+        'origen de la multicelularidad.',
+        'la continuidad de la vida en las condiciones actuales: teoría celular.'
+    ]
+    return render(request,'educ/publica/bio1eje1.html',{'lista_temas': lista_temas})
+
+
 
 def bio1eje2(request):
     return render(request,'educ/publica/bio1eje2.html')
+
 
 def bio1eje3(request):
     return render(request,'educ/publica/bio1eje3.html')
